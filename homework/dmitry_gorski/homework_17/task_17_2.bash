d725dcec0726f75e96b6357cdf17cafce77d9fa1
dhorski <dhorski@gmail.com>
2022-11-22 12:32:09 +0300

    change condition from 'lt' to 'le' remove 'continue' update echo stdout

--- homework/dmitry_gorski/homework_17/task_17_2.bash
+++ homework/dmitry_gorski/homework_17/task_17_2.bash
@@ -2,11 +2,10 @@
 
 while :
 do
-  echo -e "Введите слово: "
-  read -r user_input
-  if  [[ $user_input = '.' ]]; then
+  read -rp "Введите слово: "
+  if  [[ ${REPLY} = '.' ]]; then
     echo -e 'Вы благополучно вышли' && break
   else
-    if [[ ${#user_input} -lt 5 ]]; then echo -e 'Ok' && continue; else echo -e 'Слово слишком длинное' && continue; fi
+    if [[ ${#REPLY} -le 5 ]]; then echo -e 'Ok'; else echo -e "Слово \033[0;31m${REPLY}\033[0m слишком длинное"; fi
   fi
 done
