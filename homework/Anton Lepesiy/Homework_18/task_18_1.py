81683eeea8f30dc96ebe6af9a0a0d386ace016c1
Anton Lepesiy <anton.lepesiy@gmail.com>
2022-11-29 11:05:08 +0100

    homework 18 completed

---
+++ homework/Anton Lepesiy/Homework_18/task_18_1.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def open_browser():
+    options = Options()
+    options.add_argument('start-maximized')
+    options.add_experimental_option("detach", True)
+    chrome_driver = webdriver.Chrome(options=options)
+    sleep(2)
+    return chrome_driver
+
+
+def find_by_id(driver):
+    driver.get('https://demo.seleniumeasy.com/basic-checkbox-demo.html')
+    checkbox_element = driver.find_element(By.ID, 'isAgeSelected')
+    sleep(2)
+    checkbox_element.click()
+    sleep(2)
+    checkbox_element_after_click = driver.find_element(By.ID, 'txtAge')
+    print(checkbox_element_after_click.text)
+
+
+common_driver = open_browser()
+find_by_id(common_driver)
+common_driver.quit()
