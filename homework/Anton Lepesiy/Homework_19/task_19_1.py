5b8ef787a5b5e5cd95d22d95c39bff85772f7154
Anton Lepesiy <anton.lepesiy@gmail.com>
2022-12-04 22:14:41 +0100

    Home Work 19, file edited

--- homework/Anton Lepesiy/Homework_19/task_19_1.py
+++ homework/Anton Lepesiy/Homework_19/task_19_1.py
@@ -6,11 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import Select
-
-
 def open_browser():
     options = Options()
     options.add_argument('start-maximized')
