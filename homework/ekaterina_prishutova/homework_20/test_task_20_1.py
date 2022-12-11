48168c10b8a961d20c6eb125d8064b3304f8c190
Ekaterina Prishutova <prishutova.ea@mail.ru>
2022-12-11 18:42:17 +0300

    homework_20 done

---
+++ homework/ekaterina_prishutova/homework_20/test_task_20_1.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+import pytest
+
+
+@pytest.fixture(scope="session")
+def prepare():
+    print('before all tests')
+    yield None
+    print('\nafter all tests')
+
+
+@pytest.fixture(scope="function")
+def fixt_for_func():
+    print('before test')
+    yield None
+    print('\nafter test')
+
+
+def test_one(prepare):
+    print('test_one')
+    assert 2 == 2
+
+
+def test_two():
+    print('test_two')
+    assert 2 == 2
+
+
+@pytest.mark.simple
+def test_three(prepare, fixt_for_func):
+    print('test_three')
+    assert 2 == 2
+
+
+@pytest.mark.simple
+def test_four(fixt_for_func):
+    print('test_four')
+    assert 2 == 2
+
+
+@pytest.mark.hard
+def test_five(prepare):
+    print('test_five')
+    assert 2 == 2
+
+
+def test_six(fixt_for_func):
+    print('test_six')
+    assert 2 == 2
+
+
+@pytest.mark.skip('Slow UI test')
+def test_seven():
+    print('test_seven')
+    assert 2 == 2
+
+
+@pytest.mark.skipif(datetime.now().year == 2022, reason='reason')
+@pytest.mark.simple
+def test_eight(fixt_for_func):
+    print('test_eight')
+    assert 2 == 2
+
+
+def test_nine():
+    print('test_nine')
+    assert 2 == 2
+
+
+CREDS = [
+    ('cred1', 'pass1'),
+    ('cred2', 'pass2'),
+    ('cred3', 'pass3')
+]
+
+
+@pytest.mark.hard
+@pytest.mark.parametrize('creds', CREDS)
+def test_ten(creds):
+    print('test_ten')
+    print(creds[0], '   ', creds[1])
+    assert 2 == 2
