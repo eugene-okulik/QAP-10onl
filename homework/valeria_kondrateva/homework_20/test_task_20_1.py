334b8d86951d7a4292d202b5e747eaec878518aa
Valeria Kondrateva <llleeera@gmail.com>
2022-12-12 14:19:41 +0300

    rename file and add fixture for all tests

--- homework/valeria_kondrateva/homework_20/test_task_20_1.py
+++ homework/valeria_kondrateva/homework_20/test_task_20_1.py
@@ -20,7 +20,7 @@ def test_one(all_testing_sessions, one_test):
     assert 2 == 2
 
 
-def test_two(one_test):
+def test_two(all_testing_sessions, one_test):
     assert 1 + 1 == 2
 
 
@@ -32,36 +32,36 @@ NUMS = [
 
 
 @pytest.mark.parametrize('nums', NUMS)
-def test_three(one_test, nums):
+def test_three(all_testing_sessions, one_test, nums):
     a, b = nums
     assert a + b == 3
 
 
-def test_four(one_test):
+def test_four(all_testing_sessions, one_test):
     assert 2 + 2 == 4
 
 
 @pytest.mark.hard
-def test_five(one_test):
+def test_five(all_testing_sessions, one_test):
     assert 4 == 4
 
 
-def test_six(one_test):
+def test_six(all_testing_sessions, one_test):
     assert 4 + 4 == 8
 
 
-def test_seven(one_test):
+def test_seven(all_testing_sessions, one_test):
     assert 8 == 8
 
 
-def test_eight(one_test):
+def test_eight(all_testing_sessions, one_test):
     assert 8 + 8 == 16
 
 
-def test_nine(one_test):
+def test_nine(all_testing_sessions, one_test):
     assert 9 + 9 == 18
 
 
 @pytest.mark.skip('Slow UI test')
-def test_ten(one_test):
+def test_ten(all_testing_sessions, one_test):
     assert 10 + 10 == 20
