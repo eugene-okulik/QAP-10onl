dd1bbe76512f7afaed522afe286401024922931c
Alesia Kurylenka <alesia.kurylenka@gmail.com>
2022-12-15 19:26:39 +0300

    removed duplicate data in eco_friendly_page.py

--- homework/alesia_kurylenka/homework_22/magento_project/pages/eco_friendly_page.py
+++ homework/alesia_kurylenka/homework_22/magento_project/pages/eco_friendly_page.py
@@ -11,14 +11,8 @@ class EcoFriendlyPage(BasePage):
         assert self.find(loc.page_title).text == "Eco Friendly"
         return self.driver.current_url == self.page_url
 
-    def is_12_products_on_the_page(self):
-        return len(self.find_all(loc.products_list)) == 12
-
     def products_count(self):
         return len(self.find_all(loc.products_list))
-
-    def is_12_with_lists_on_the_page(self):
-        return len(self.find_all(loc.add_to_with_list)) == 12
 
     def wish_lists_count(self):
         return len(self.find_all(loc.add_to_with_list))
