94fbd8159ed6ed9526bb97a42539ba1f6f4106ee
Evgeny Sporshev <verygnawtyatwork@yandex.ru>
2022-12-17 00:37:19 +0300

    Evgeny Sporshev homework 20 checking mistakes

--- homework/evgeny_sporshev/homework_20/test_hw_20.py
+++ homework/evgeny_sporshev/homework_20/test_hw_20.py
@@ -22,41 +22,41 @@ def test_one(for_all_test, for_current_test):
     assert 2 == 2
 
 
-def test_two(for_current_test):
+def test_two(for_all_test, for_current_test):
     assert 3 == 3
 
 
-def test_three(for_current_test):
+def test_three(for_all_test, for_current_test):
     assert 'word' == 'word'
 
 
-def test_four():
+def test_four(for_all_test):
     assert 2 + 2 == 4
 
 
-def test_five():
+def test_five(for_all_test):
     assert 'q' == 'q'
 
 
-def test_six():
+def test_six(for_all_test):
     assert 'six' == 'six'
 
 
 @pytest.mark.hard
-def test_seven():
+def test_seven(for_all_test):
     assert '123' == '123'
 
 
 @pytest.mark.slow
-def test_eight():
+def test_eight(for_all_test):
     assert 5 == 5
 
 
 @pytest.mark.parametrize('word', TEST_DATA)
-def test_nine(word):
+def test_nine(for_all_test, word):
     assert word == 'Two'
 
 
 @pytest.mark.skip("Test haven't done yet")
-def test_ten():
+def test_ten(for_all_test):
     assert 'hello' == 'hello'
