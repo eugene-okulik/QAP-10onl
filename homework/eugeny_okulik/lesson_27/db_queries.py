fa2512c9a979356584bf23b0adb736e078908c99
eugeny.okulik <tiburon854@gmail.com>
2023-01-10 09:22:22 +0300

    Fixed identation

--- homework/eugeny_okulik/lesson_27/db_queries.py
+++ homework/eugeny_okulik/lesson_27/db_queries.py
@@ -2,11 +2,11 @@ import mysql.connector as mysql
 
 
 db = mysql.connect(
-   host='db-mysql-fra1-64949-do-user-7651996-0.b.db.ondigitalocean.com',
-   port=25060,
-   user='user1',
-   passwd='AVNS_CBt-mC3cbjNrlcTNBr_',
-   database='qap09'
+    host='db-mysql-fra1-64949-do-user-7651996-0.b.db.ondigitalocean.com',
+    port=25060,
+    user='user1',
+    passwd='AVNS_CBt-mC3cbjNrlcTNBr_',
+    database='qap09'
 )
 
 cursor = db.cursor(dictionary=True)
