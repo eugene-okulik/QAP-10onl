347ae1a6b715b5e81eebbdd16762372fa5a8f5d8
AmelchenkoDima <amelchencko.dima1998@gmail.com>
2023-01-11 15:55:32 +0300

    hw_25.1.py

--- homework/Dima_Amelchenko/HW_25/conftest.py
+++ homework/Dima_Amelchenko/HW_25/conftest.py
@@ -2,7 +2,6 @@ import pytest
 import json
 import requests
 
-
 DOMAIN = 'http://167.172.172.115:52355'
 TOKEN = open('token.txt', 'r').read()
 
@@ -23,20 +22,9 @@ def authorize(domain, token):
     if response == "Token is alive. Username is Dima_Amelchenko":
         return token
     else:
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        data = json.dumps(
-            {
-                "name": "Dima_Amelchenko"
-            }
-        )
-        response = requests.request(
-            'POST',
-            f'{domain}/authorize',
-            headers=headers,
-            data=data
-        ).json()
+        headers = {'Content-Type': 'application/json'}
+        data = json.dumps({"name": "Dima_Amelchenko"})
+        response = requests.request('POST', f'{domain}/authorize', headers=headers, data=data).json()
         with open("token.txt", "w") as f:
             f.write(response["token"])
         return token
@@ -44,30 +32,12 @@ def authorize(domain, token):
 
 @pytest.fixture(scope='function')
 def add_meme(domain, authorize):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'{authorize}'
-    }
-    data = json.dumps(
-        {
-            "text": "Family",
-            "url":
-                "https://www.thevoicemag.ru/upload/img_cache/f0e/f0e1c3b4b532fbc70a73e022ffcf35f2_fitted_1332x0.jpg",
-            "tags": [
-                "fun",
-                "Vin Diesel"
-            ],
-            "info": {
-                "type": "jpg"
+    headers = {'Content-Type': 'application/json', 'Authorization': f'{authorize}'}
+    data = json.dumps({"text": "Family",
+        "url": "https://www.thevoicemag.ru/upload/img_cache/f0e/f0e1c3b4b532fbc70a73e022ffcf35f2_fitted_1332x0.jpg",
+        "tags": ["fun", "Vin Diesel"], "info": {"type": "jpg"
 
-            }
-        }
-    )
-    response = requests.request(
-        'POST',
-        f'{domain}/meme',
-        headers=headers,
-        data=data
-    ).json()
+        }})
+    response = requests.request('POST', f'{domain}/meme', headers=headers, data=data).json()
     meme_id = response['id']
     return meme_id
