aa3bd3a011b14b0e9ba74b2876de147522609405
AmelchenkoDima <amelchencko.dima1998@gmail.com>
2023-01-12 10:36:38 +0300

    hw_25.3.py

--- homework/Dima_Amelchenko/HW_25/conftest.py
+++ homework/Dima_Amelchenko/HW_25/conftest.py
@@ -22,8 +22,14 @@ def authorize(domain, token):
     if response == "Token is alive. Username is Dima_Amelchenko":
         return token
     else:
-        headers = {'Content-Type': 'application/json'}
-        data = json.dumps({"name": "Dima_Amelchenko"})
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = json.dumps(
+            {
+                "name": "Dima_Amelchenko"
+            }
+        )
         response = requests.request('POST', f'{domain}/authorize', headers=headers, data=data).json()
         with open("token.txt", "w") as f:
             f.write(response["token"])
@@ -32,12 +38,20 @@ def authorize(domain, token):
 
 @pytest.fixture(scope='function')
 def add_meme(domain, authorize):
-    headers = {'Content-Type': 'application/json', 'Authorization': f'{authorize}'}
-    data = json.dumps({"text": "Family",
-        "url": "https://www.thevoicemag.ru/upload/img_cache/f0e/f0e1c3b4b532fbc70a73e022ffcf35f2_fitted_1332x0.jpg",
-        "tags": ["fun", "Vin Diesel"], "info": {"type": "jpg"
-
-        }})
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'{authorize}'
+    }
+    data = json.dumps(
+        {
+            "text": "Family",
+            "url": "https://www.thevoicemag.ru/upload/img_cache/f0e/f0e1c3b4b532fbc70a73e022ffcf35f2_fitted_1332x0.jpg",
+            "tags": ["fun", "Vin Diesel"],
+            "info": {
+                "type": "jpg"
+            }
+        }
+    )
     response = requests.request('POST', f'{domain}/meme', headers=headers, data=data).json()
     meme_id = response['id']
     return meme_id
